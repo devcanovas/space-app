2a1ca96bd7478cfb5993cf6567ce99a325b0362f
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-10 17:50:40 -0300

    Refatorando algumas funcionalidades

--- src/context/GaleriaContext.jsx
+++ src/context/GaleriaContext.jsx
@@ -8,6 +8,12 @@ export const GaleriaProvider = ({ children }) => {
   const [fotoSelecionada, setFotoSelecionada] = useState(null);
 
   const aoAlternarFavorito = (foto) => {
+    if(foto.id === fotoSelecionada?.id) {
+      setFotoSelecionada({
+        ...fotoSelecionada,
+        favorita: !fotoSelecionada.favorita
+      })
+    }
     setFotosDaGaleria(fotosDaGaleria.map((fotoDaGaleria) => {
       return {
         ...fotoDaGaleria,
